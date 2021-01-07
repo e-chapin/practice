d660570eb2b95e054a31d363374f0bb8e07f7f3e
Eric Jameson <e-jameson@bethel.edu>
2021-01-06 18:45:17 -0600

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Button from "./components/button";
 
 function App() {
   return (
     <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
       <h1>cool page</h1>
-      <Button onClick={() => console.log("click")}>I am Button</Button>
+      {/* <Button onClick={() => console.log("click")}>I am Button</Button> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
-import App from "../App";
-import * as serviceWorker from "./serviceWorker";
-import reportWebVitals from "../reportWebVitals";
+import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-serviceWorker.unregister();
+// serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
